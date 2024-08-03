fa1e22df23c37b032cdac5ef584af4f23531439f
Michaecyber <oflenstek@gmal.com>
2024-08-03 18:48:57 +0200

    first commit

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development' ? 'https://bigbox-api.onrender.com/' : '/',
+    process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : '/',
   headers: {
     'Content-type': 'application/json',
   },
